e1c2ca80e2eadaeaefd0d1f586eda0b9c41a8550
Max Gunton <maxgunton@hotmail.com>
2023-11-21 11:40:01 -0800

    Aliased package hydr as hydrophone for backwards compatibility

--- hydr/utils.py
+++ hydr/utils.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(os.path.dirname(SCRIPT_DIR))
 # from this package
+import hydr as hydrophone  # for backward compatibility when unpickling
 from hydr.definitions import (ContainsRestrictedCharacter, ALLOWED_FILENAME_CHARACTERS,
                               DEVICES)
-
 
 # DECORATOR for classes to make them singleton
 def singleton(class_):
